581f19a18cf6a6dc090c5e489edeaeee00f37d22
JoaquinEG98 <joaquin4525@hotmail.com>
2024-08-16 21:30:21 -0300

    Separo el navbar del site master

--- MidMarket.UI/Site.Master.designer.cs
+++ MidMarket.UI/Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace MidMarket.UI
     {
 
         /// <summary>
+        /// NavbarControl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::MidMarket.UI.Navbar NavbarControl;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
