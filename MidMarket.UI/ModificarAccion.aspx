a3c2e5d712dd4b75268076f442b3b27208c0b430
JoaquinEG98 <joaquin4525@hotmail.com>
2024-10-20 21:04:10 -0300

    Modificacion de acciones

---
+++ MidMarket.UI/ModificarAccion.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ModificarAccion.aspx.cs" Inherits="MidMarket.UI.ModificarAccion" MasterPageFile="~/Site.Master" Title="Modificar Acción" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container-acciones">
+        <form class="registro-form" method="post">
+            <h2>Modificar Acción</h2>
+
+            <div class="form-group-acciones">
+                <label for="nombreAccion">Nombre de la Acción</label>
+                <input type="text" id="nombreAccion" name="nombreAccion" value="<%= Accion.Nombre %>" class="form-control" required>
+            </div>
+
+            <div class="form-group-acciones">
+                <label for="simboloAccion">Símbolo de la Acción</label>
+                <input type="text" id="simboloAccion" name="simboloAccion" value="<%= Accion.Simbolo %>" class="form-control" required>
+            </div>
+
+            <div class="form-group-acciones">
+                <label for="precioAccion">Precio de la Acción</label>
+                <input type="text" id="precioAccion" name="precioAccion" value="<%= Accion.Precio.ToString("F2") %>" class="form-control" required>
+            </div>
+
+            <asp:Button ID="btnGuardar" runat="server" Text="Guardar" CssClass="submit-btn-acciones" OnClick="btnGuardar_Click" />
+        </form>
+    </div>
+</asp:Content>
---
+++ MidMarket.UI/ModificarAccion.aspx.cs
@@ -0,0 +1,77 @@
+﻿using MidMarket.Business.Interfaces;
+using MidMarket.Entities;
+using MidMarket.UI.Helpers;
+using System;
+using System.Linq;
+using Unity;
+
+namespace MidMarket.UI
+{
+    public partial class ModificarAccion : System.Web.UI.Page
+    {
+        private readonly IActivoService _activoService;
+        public Accion Accion { get; set; }
+        private int _accionId { get; set; }
+
+        public ModificarAccion()
+        {
+            _activoService = Global.Container.Resolve<IActivoService>();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _accionId = int.Parse(Request.QueryString["id"]);
+                CargarAccion();
+            }
+            catch (Exception ex)
+            {
+                AlertHelper.MostrarMensaje(this, $"Error al cargar la página: {ex.Message}");
+                Response.Redirect("AdministrarAcciones.aspx");
+            }
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombreAccion = Request.Form["nombreAccion"];
+                string simboloAccion = Request.Form["simboloAccion"];
+                decimal precioAccion = decimal.Parse(Request.Form["precioAccion"]);
+
+                GuardarAccion(nombreAccion, simboloAccion, precioAccion);
+                AlertHelper.MostrarMensaje(this, $"Acción {nombreAccion} modificada correctamente.");
+                CargarAccion();
+            }
+            catch (Exception ex)
+            {
+                AlertHelper.MostrarMensaje(this, $"Error al modificar la acción: {ex.Message}");
+            }
+        }
+
+        private void GuardarAccion(string nombreAccion, string simboloAccion, decimal precioAccion)
+        {
+            var accion = new Accion()
+            {
+                Id = Accion.Id,
+                Id_Accion = Accion.Id_Accion,
+                Nombre = nombreAccion,
+                Simbolo = simboloAccion,
+                Precio = precioAccion
+            };
+
+            _activoService.ModificarAccion(accion);
+        }
+
+        private void CargarAccion()
+        {
+            Accion = _activoService.GetAcciones().FirstOrDefault(x => x.Id == _accionId);
+            if (Accion == null)
+            {
+                AlertHelper.MostrarMensaje(this, "Acción no encontrada.");
+                Response.Redirect("AdministrarAcciones.aspx");
+            }
+        }
+    }
+}
