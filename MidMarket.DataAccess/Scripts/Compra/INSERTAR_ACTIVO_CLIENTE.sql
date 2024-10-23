9824a09ef76d6747de147b44cc76cb4ff858d9e9
JoaquinEG98 <joaquin4525@hotmail.com>
2024-10-23 16:41:28 -0300

    fix en la compra

--- MidMarket.DataAccess/Scripts/Compra/INSERTAR_ACTIVO_CLIENTE.sql
+++ MidMarket.DataAccess/Scripts/Compra/INSERTAR_ACTIVO_CLIENTE.sql
@@ -1,3 +1,13 @@
-﻿INSERT INTO Cliente_Activo (Id_Cliente, Id_Activo, Cantidad)
-OUTPUT inserted.Id_Cliente_Activo
-VALUES (@Id_Cliente, @Id_Activo, @Cantidad)
+﻿IF EXISTS (SELECT 1 FROM Cliente_Activo WHERE Id_Cliente = @Id_Cliente AND Id_Activo = @Id_Activo)
+BEGIN
+    UPDATE Cliente_Activo
+    SET Cantidad = Cantidad + @Cantidad
+    OUTPUT inserted.Id_Cliente_Activo
+    WHERE Id_Cliente = @Id_Cliente AND Id_Activo = @Id_Activo;
+END
+ELSE
+BEGIN
+    INSERT INTO Cliente_Activo (Id_Cliente, Id_Activo, Cantidad)
+    OUTPUT inserted.Id_Cliente_Activo
+    VALUES (@Id_Cliente, @Id_Activo, @Cantidad);
+END
