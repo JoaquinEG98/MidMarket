7ee7c0990ba34645eecc9e83e17f33ac7159127b
JoaquinEG98 <joaquin4525@hotmail.com>
2024-10-27 11:16:55 -0300

    Se separa la logica de creacion de XML a la capa correspondiente

---
+++ MidMarket.XML/ComprasXML.cs
@@ -0,0 +1,39 @@
+﻿using MidMarket.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MidMarket.XML
+{
+    public static class ComprasXML
+    {
+        public static void GenerarXMLActivosPorCantidad(List<ActivosCompradosDTO> activos)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<ActivosCompradosDTO>));
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, activos);
+                string xmlResult = writer.ToString();
+
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "ActivosCompradosCantidad.xml");
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+                File.WriteAllText(path, xmlResult);
+            }
+        }
+
+        public static void GenerarXMLActivosPorTotal(List<ActivosCompradosDTO> activos)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<ActivosCompradosDTO>));
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, activos);
+                string xmlResult = writer.ToString();
+
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "ActivosCompradosTotal.xml");
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+                File.WriteAllText(path, xmlResult);
+            }
+        }
+    }
+}
