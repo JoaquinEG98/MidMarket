58186e68cfbb1db5b86d66c3dcaf2f3126dd05fd
JoaquinEG98 <joaquin4525@hotmail.com>
2024-11-03 11:40:45 -0300

    Se agrega cache para las traducciones

--- MidMarket.DataAccess/DAOs/TraduccionDAO.cs
+++ MidMarket.DataAccess/DAOs/TraduccionDAO.cs
@@ -1,22 +1,32 @@
 ﻿using MidMarket.DataAccess.Interfaces;
 using MidMarket.Entities.Observer;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text;
 
 namespace MidMarket.DataAccess.DAOs
 {
     public class TraduccionDAO : ITraduccionDAO
     {
+        private static readonly MemoryCache _cache = MemoryCache.Default;
+        private const string _cacheKey = "Traducciones";
+        private readonly DateTimeOffset _tiempoCache = DateTimeOffset.Now.AddHours(1);
+
         public IList<Idioma> ObtenerIdiomas()
         {
-            string json = Encoding.UTF8.GetString(Traduccion.Idioma);
+            if (!_cache.Contains(_cacheKey))
+            {
+                string json = Encoding.UTF8.GetString(Traduccion.Idioma);
+                IList<Idioma> idiomas = JsonConvert.DeserializeObject<IList<Idioma>>(json);
 
-            IList<Idioma> idiomas = JsonConvert.DeserializeObject<IList<Idioma>>(json);
+                _cache.Set(_cacheKey, idiomas, _tiempoCache);
+            }
 
-            return idiomas;
+            return (IList<Idioma>)_cache.Get(_cacheKey);
         }
 
         public IIdioma ObtenerIdiomaDefault()
@@ -26,15 +36,23 @@ namespace MidMarket.DataAccess.DAOs
 
         public IDictionary<string, ITraduccion> ObtenerTraducciones(IIdioma idioma)
         {
+            if (!_cache.Contains(_cacheKey))
+            {
+                string json = Encoding.UTF8.GetString(Traduccion.Traducciones);
+                IList<Entities.Observer.Traduccion> traducciones = JsonConvert.DeserializeObject<IList<Entities.Observer.Traduccion>>(json);
+
+                var cacheData = traducciones.ToDictionary(t => $"{t.IdiomaId}_{t.Etiqueta}", t => (ITraduccion)t);
+                _cache.Set(_cacheKey, cacheData, _tiempoCache);
+            }
+
             if (idioma == null)
                 idioma = ObtenerIdiomaDefault();
 
-            string json = Encoding.UTF8.GetString(Traduccion.Traducciones);
-            IList<Entities.Observer.Traduccion> traducciones = JsonConvert.DeserializeObject<IList<Entities.Observer.Traduccion>>(json);
+            var traduccionesCache = (IDictionary<string, ITraduccion>)_cache.Get(_cacheKey);
 
-            var traduccionesFiltradas = traducciones
-                .Where(t => t.IdiomaId == idioma.Id)
-                .ToDictionary(t => t.Etiqueta, t => (ITraduccion)t);
+            var traduccionesFiltradas = traduccionesCache
+                .Where(t => t.Key.StartsWith($"{idioma.Id}_"))
+                .ToDictionary(t => t.Key.Split('_')[1], t => t.Value);
 
             return traduccionesFiltradas;
         }
